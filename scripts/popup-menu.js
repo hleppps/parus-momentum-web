3168b745e0b6aab7a9a08f36466d7215b0ff6a7a
hleppps <ddddffffgggg36@gmail.com>
2021-03-22 11:42:01 +0200

    test

---
+++ scripts/popup-menu.js
@@ -0,0 +1,12 @@
+function popUpMenu () {
+	let menu = document.querySelector('.m-menu-content')
+	// let body = document.body
+	// body.style.overflow-y : 'hidden'
+
+	menu.style.transform = 'none'
+}
+
+function ppopUpMenu () {
+	let menu = document.querySelector('.m-menu-content')
+	menu.style.transform = 'translate(120%, 0)'
+}
