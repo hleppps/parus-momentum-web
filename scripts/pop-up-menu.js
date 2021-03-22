8cb4a40427a386894e8ab364661a969d757cc7a4
hleppps <ddddffffgggg36@gmail.com>
2021-03-22 15:18:01 +0200

    pop-up menu fix (body scroll)

--- scripts/pop-up-menu.js
+++ scripts/pop-up-menu.js
@@ -3,8 +3,16 @@ let is_menu_open = false
 function popUpMenu () {
 	let pop_up_menu = document.querySelector('.m-popUpMenu')
 	let pop_up_menu_img = document.querySelector('.a-popUpMenuImage')
+	let body = document.body
 
-	!is_menu_open ? openPopUpMenu(pop_up_menu, pop_up_menu_img) : closePopUpMenu(pop_up_menu, pop_up_menu_img)
+	if (!is_menu_open) {
+		body.style.overflowY = 'hidden'
+
+		openPopUpMenu(pop_up_menu, pop_up_menu_img)
+	} else {
+		body.style.overflowY = 'scroll'
+		closePopUpMenu(pop_up_menu, pop_up_menu_img)
+	}
 }
 
 function openPopUpMenu (menu, img) {
