9dad5f059bdea97da9b4ffb4a76132d321a6feb5
hleppps <ddddffffgggg36@gmail.com>
2021-03-22 21:59:35 +0200

    cleaned event-handling.js

--- scripts/event-handling.js
+++ scripts/event-handling.js
@@ -1,10 +1,4 @@
 function handleEvent() {
 	let pop_up_menu_btn = document.querySelector('.a-popUpMenuButton')
 	pop_up_menu_btn.addEventListener('click', popUpMenu)
-
-	// let pop_up_menu_btn_close = document.querySelector('.a-popUpMenuButton-close')
-	// pop_up_menu_btn_close.addEventListener('click', closePopUpMenu)
-
-   // let check_email_validity = document.querySelector('.a-input-email')
-   // check_email_validity.addEventListener('keyup', checkEmailValidity)
 }
